7b1927ab6721fe1098e0a42b8aceff478a561178
RaymondLi <lalalic@gmail.com>
2013-05-09 12:19:35 +0800

    make android app's first version simple to load cache only

--- base/android/src/com/yy/m/view/OfflineWebViewClient.java
+++ base/android/src/com/yy/m/view/OfflineWebViewClient.java
@@ -1,10 +1,6 @@
 package com.yy.m.view;
 
-import java.util.regex.Pattern;
-
 import android.content.Context;
-import android.util.Log;
-import android.webkit.WebView;
 
 public class OfflineWebViewClient extends OnlineWebViewClient{
 
@@ -12,20 +8,4 @@ public class OfflineWebViewClient extends OnlineWebViewClient{
 		super(ctx);
 	}
 
-	@Override
-	public boolean shouldOverrideUrlLoading(WebView view, String url) {
-		Pattern pattern=this.getPattern(url);
-		if(pattern!=null){
-			((WebViewEx)view).loadOffline(pattern.pattern());
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	public void onLoadResource(WebView view, String url) {
-		Log.d("Offline", url);
-	}
-	
-	
 }
