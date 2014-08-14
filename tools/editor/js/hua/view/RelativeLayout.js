eb5d52d56a790c6f4cbfa257b4a95d396c734eb0
RaymondLi <lalalic@gmail.com>
2014-08-14 10:19:14 +0800

    clear tool

--- tools/editor/js/hua/view/RelativeLayout.js
+++
@@ -1,8 +0,0 @@
-define(['view/ViewGroup'],function(ViewGroup){
-	var RelativeLayout=function(){
-		ViewGroup.call(this,arguments);
-	};
-	this.RelativeLayout=RelativeLayout;
-	RelativeLayout.prototype=new ViewGroup();
-	return RelativeLayout;
-})
--- tools/editor/js/hua/view/testRelativeLayout.js
+++
@@ -1,7 +0,0 @@
-define(['view/RelativeLayout'],function(RelativeLayout){
-	describe('Test RelativeLayout',function(){
-		it('hello',function(){
-			expect(true).toBe(true)
-		})
-	})
-})
